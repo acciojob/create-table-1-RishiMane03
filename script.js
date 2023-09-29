a851d725d64275666e0595e3df67fe3768d874ea
Rishikesh Mane <139518215+RishiMane03@users.noreply.github.com>
2023-09-29 18:05:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
+var table = document.getElementById("sampleTable");
+
 function insert_Row() {
     //Write your code here
-  
-  
+	  // Create a new row element
+            var newRow = table.insertRow(0); // 1 indicates the position (top) to insert the row
+
+            // Create cells for the new row
+            var cell1 = newRow.insertCell(0);
+            var cell2 = newRow.insertCell(1);
+
+            // Populate the cells with data
+            cell1.innerHTML = "New Cell1";
+            cell2.innerHTML = "New Cell2";
 }
